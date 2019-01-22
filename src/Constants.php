cc323e19959b5308f2e092f9f3f2482ca04a0d91
Simon Mellerin <simon.mellerin@makina-corpus.com>
2019-01-22 14:53:50 +0100

    Handle global constants in a Constants Class

---
+++ src/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class Constants {
+
+    /**
+     * Current version
+     * @var \String
+     */
+    public const VERSION = '0.3.0';
+
+    /**
+     * Github repo URL
+     * @var \String
+     */
+    public const GITHUB = 'https://github.com/SimonMellerin/Pilea';
+}
