9c9f8855d0f373ea5719df72207d8fc7f92dbc30
Simon Mellerin <simon.mellerin@makina-corpus.com>
2019-01-22 14:54:25 +0100

    Add Command pilea:version to retrieve version

---
+++ src/Command/VersionCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Constants;
+
+/***
+ * Inspired by Wallabag install command.
+ */
+class VersionCommand extends Command
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('pilea:version')
+            ->setDescription('Get Pilea version.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->write(Constants::VERSION);
+    }
+}
