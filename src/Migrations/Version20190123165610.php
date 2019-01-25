a56e336c3623fd0a8b4e56b0f5ebefde73b51f7e
Simon Mellerin <simon.mellerin@makina-corpus.com>
2019-01-25 15:31:46 +0100

    Migrations: add initial migration to init database

---
+++ src/Migrations/Version20190123165610.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Initial Migration, nothing to do there !
+ */
+final class Version20190123165610 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // Pouet pouet
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf(true, 'Always move forward.');
+
+    }
+}
