a319095999acf14d2fbc43aa19f22b57f884d3be
Simon Mellerin <simon.mellerin@makina-corpus.com>
2019-10-10 14:41:07 +0200

    Fix migration

--- src/Migrations/Version20190917091046.php
+++ src/Migrations/Version20190917091046.php
@@ -25,7 +25,7 @@ final class Version20190917091046 extends AbstractMigration
         $this->addSql('CREATE TABLE IF NOT EXISTS user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('INSERT INTO user (username, roles, password) VALUES ("admin", "[\"ROLE_ADMIN\"]", "$argon2i$v=19$m=65536,t=4,p=1$RUN1ZmY3RVlTYU5iYU9xdw$TKXm66YFI4P9+AcDjvxGB6UwwC6COsjhrH280QAQIC8")');
 
-        $this->addSql('ALTER TABLE place DROP COLUMN creator_id');
+        $this->addSql('ALTER TABLE place DROP COLUMN IF EXISTS creator_id');
         $this->addSql('ALTER TABLE place ADD COLUMN user_id INT');
         $this->addSql('ALTER TABLE place ADD CONSTRAINT FK_741D53CDA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_741D53CDA76ED395 ON place (user_id)');
