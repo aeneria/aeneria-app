38468ec33707dc66c751a5098a2df8544273a44d
Simon Mellerin <simon.mellerin@makina-corpus.com>
2020-02-28 14:30:57 +0100

    fix migration

--- src/Migrations/Version20200206090202.php
+++ src/Migrations/Version20200206090202.php
@@ -23,7 +23,8 @@ final class Version20200206090202 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE place DROP FOREIGN KEY IF EXISTS FK_741D53CDA76ED395');
-        $this->addSql('ALTER TABLE place_user DROP FOREIGN KEY IF EXISTS  FK_4726A6A5A76ED395');
+        $this->addSql('DROP INDEX IF EXISTS UNIQ_1483A5E9F85E0677 ON user');
+        $this->addSql('ALTER TABLE place_user DROP FOREIGN KEY IF EXISTS FK_4726A6A5A76ED395');
         $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, active TINYINT(1) DEFAULT \'1\' NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('INSERT INTO users SELECT * FROM user');
         $this->addSql('DROP TABLE IF EXISTS  user');
