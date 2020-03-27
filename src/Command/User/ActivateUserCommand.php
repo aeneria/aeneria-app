SimonMellerin/Pilea

from pilea to aeneria

src/Command/User/ActivateUserCommand.php
@@ -39,7 +39,7 @@ class ActivateUserCommand extends Command
     protected function configure()
     {
         $this
-            ->setName('pilea:user:deactivate')
+            ->setName('aeneria:user:deactivate')
             ->setDescription('Activate an user.')
             ->addArgument('username', InputArgument::REQUIRED, 'Username')
         ;

aeneria/aeneria-app

src/Command/User/ActivateUserCommand.php
@@ -38,7 +38,7 @@ class ActivateUserCommand extends Command
     protected function configure()
     {
         $this
-            ->setName('pilea:user:activate')
+            ->setName('aeneria:user:activate')
             ->setDescription('Activate an user.')
             ->addArgument('username', InputArgument::REQUIRED, 'Username')
         ;