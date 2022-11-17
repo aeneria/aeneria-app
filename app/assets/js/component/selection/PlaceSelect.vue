8e71827bf615926c6edc4028a680f73c4d3be145
Simon Mellerin <simon.mellerin@makina-corpus.com>
2022-11-17 18:59:19 +0100

    Fix webpack config

--- app/assets/js/component/selection/PlaceSelect.vue
+++ app/assets/js/component/selection/PlaceSelect.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="select-place p-d-flex p-ai-center">
     <p class="p-pr-2">{{ place?.name }}</p>
-    <Button
-      v-if="placeList.length > 1"
-      icon="pi pi-directions"
-      title="Sélectionner une autre adresse"
-      class="p-button-rounded p-button-secondary p-button-icon"
-      @click="openDialog()"
-    />
-    <Dialog
-      header="Sélectionner une autre adresse"
-      v-model:visible="displayDialog"
-      :style="{'max-width': '90vw'}"
-    >
-      <div class="p-d-flex p-jc-center p-flex-wrap">
-        <template v-for="placeItem of placeList" :key="placeItem.id">
-          <Button
-            class="p-button p-button-rounded p-button-secondary p-mr-1 p-mb-1"
-            :label="placeItem.name"
-            @click="setSelectedPlace(placeItem)"
-          />
-        </template>
-      </div>
-    </Dialog>
+    <template v-if="placeList">
+      <Button
+        v-if="!onlyOnePlace"
+        icon="pi pi-directions"
+        title="Sélectionner une autre adresse"
+        class="p-button-rounded p-button-secondary p-button-icon"
+        @click="openDialog()"
+      />
+      <Dialog
+        header="Sélectionner une autre adresse"
+        v-model:visible="displayDialog"
+        :style="{'max-width': '90vw'}"
+      >
+        <div class="p-d-flex p-jc-center p-flex-wrap">
+          <template v-for="placeItem of placeList" :key="placeItem.id">
+            <Button
+              class="p-button p-button-rounded p-button-secondary p-mr-1 p-mb-1"
+              :label="placeItem.name"
+              @click="setSelectedPlace(placeItem)"
+            />
+          </template>
+        </div>
+      </Dialog>
+    </template>
   </div>
 </template>
 
