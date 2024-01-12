7cbef2af34069950ce7e23bad18075ff217aee52
Simon Mellerin <simon.mellerin@makina-corpus.com>
2024-01-12 14:52:16 +0100

    Add tests

--- app/tests/Unit/Services/DataImporterTest.php
+++ app/tests/Unit/Services/DataImporterTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Services;
 
+use App\Entity\Feed;
+use App\Entity\Place;
 use App\Entity\User;
 use App\Services\DataImporter;
 use App\Tests\AppTestCase;
@@ -53,5 +55,86 @@ final class DataImporterTest extends AppTestCase
 
         // The file contains 4 errors
         self::assertCount(4, $errors);
+    }
+
+    public function testDataImporterEnedisHourFile()
+    {
+        $dataValueRepository = $this->getDataValueRepository();
+        $entityManager = $this->getEntityManager();
+        $dataImporter = new DataImporter(
+            $dataValueRepository,
+            $entityManager,
+            $this->getLogger()
+        );
+
+        $user = $this->getUserRepository()->findOneByUsername('user-test@example.com');
+        \assert($user instanceof User);
+
+        $place = $user->getPlaces()[0];
+        \assert($place instanceof Place);
+
+        $feed = $place->getFeed(Feed::FEED_TYPE_ELECTRICITY);
+
+        $errors = $dataImporter->importFile(
+            $place,
+            $feed,
+            $this->getResourceDir() . '/Enedis_Conso_Heure.csv'
+        );
+
+        self::assertCount(0, $errors);
+    }
+
+    public function testDataImporterEnedisDayFile()
+    {
+        $dataValueRepository = $this->getDataValueRepository();
+        $entityManager = $this->getEntityManager();
+        $dataImporter = new DataImporter(
+            $dataValueRepository,
+            $entityManager,
+            $this->getLogger()
+        );
+
+        $user = $this->getUserRepository()->findOneByUsername('user-test@example.com');
+        \assert($user instanceof User);
+
+        $place = $user->getPlaces()[0];
+        \assert($place instanceof Place);
+
+        $feed = $place->getFeed(Feed::FEED_TYPE_ELECTRICITY);
+
+        $errors = $dataImporter->importFile(
+            $place,
+            $feed,
+            $this->getResourceDir() . '/Enedis_Conso_Jour.csv'
+        );
+
+        self::assertCount(0, $errors);
+    }
+
+    public function testDataImporterGrdfFile()
+    {
+        $dataValueRepository = $this->getDataValueRepository();
+        $entityManager = $this->getEntityManager();
+        $dataImporter = new DataImporter(
+            $dataValueRepository,
+            $entityManager,
+            $this->getLogger()
+        );
+
+        $user = $this->getUserRepository()->findOneByUsername('user-test@example.com');
+        \assert($user instanceof User);
+
+        $place = $user->getPlaces()[0];
+        \assert($place instanceof Place);
+
+        $feed = $place->getFeed(Feed::FEED_TYPE_GAZ);
+
+        $errors = $dataImporter->importFile(
+            $place,
+            $feed,
+            $this->getResourceDir() . '/export_grdf.xlsx'
+        );
+
+        self::assertCount(0, $errors);
     }
 }
